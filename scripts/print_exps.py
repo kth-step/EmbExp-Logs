530a74bcc9b7cab3cc6d2fe9fa3f22417b4d0017
Andreas Lindner <andili@kth.se>
2021-01-20 23:03:54 +0100

    Enable printing of single experiment also

--- scripts/print_exps.py
+++ scripts/print_exps.py
@@ -9,10 +9,12 @@ import logging
 
 import logsdb as ldb
 import logslist
+import experiment
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("listname", help="name of experiment list")
+parser.add_argument("-ln", "--listname", help="name of experiment list")
+parser.add_argument("-eid", "--exp_id", help="experiment id")
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
 
@@ -22,6 +24,14 @@ if args.verbose:
 else:
 	logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
 
+listname = args.listname
+exp_id = args.exp_id
+
+if not (listname != None or exp_id != None): 
+	raise Exception("either listname or exp_id must be set")
+
+if listname != None and exp_id != None:
+	raise Exception("only one, either listname or exp_id should be set")
 
 print("opening db...")
 print()
@@ -29,8 +39,10 @@ db = ldb.LogsDB()
 db.connect()
 
 # collect exps
-listname = args.listname
-exps = logslist.LogsList._get_by_name(db, "exp", listname).get_entries()
+if listname != None:
+	exps = logslist.LogsList._get_by_name(db, "exp", listname).get_entries()
+else:
+	exps = [(0, experiment.Experiment(db, int(exp_id)))]
 
 # print all exps
 for exp in exps:
