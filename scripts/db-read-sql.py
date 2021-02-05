4afc245ca2ffa271096ea5880b834d6c55df7baf
Andreas Lindner <andili@kth.se>
2021-02-05 03:36:47 +0100

    Add script to enable quick "reading" queries

---
+++ scripts/db-read-sql.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
+
+import argparse
+import logging
+
+import logsdb as ldb
+
+# parse arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", help="take input as command line argument instead of stdin")
+
+parser.add_argument("-t", "--testing", help="uses testing database (i.e. for testing only)", action="store_true")
+
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+args = parser.parse_args()
+
+# set log level
+if args.verbose:
+	logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+else:
+	logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
+
+input_data = args.input
+is_testing = args.testing
+
+# retrieve query string from stdin
+logging.info(f"loading query string.")
+if input_data != None:
+	sql_str = input_data
+else:
+	logging.info(f"from stdin...")
+	sql_str = sys.stdin.read()
+
+# create db access object
+alt_db_file = None if not is_testing else "data/testing.db"
+with ldb.LogsDB(alt_db_file, read_only=True) as db:
+	# execute query
+	(fields, rows) = db.get_tablerecords_sql(sql_str)
+
+# print result
+print(fields)
+print("=" * 40)
+print()
+for row in rows:
+	print(row)
+
